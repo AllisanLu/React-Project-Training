a753d846fee81ed00e9c5a8489f8f5e01590573d
AllisanLu <alcp2610@gmail.com>
2024-08-26 13:09:25 -0400

    Basic GET endpoints for Customer db

--- server/database.js
+++ server/database.js
@@ -1,16 +1,14 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import { config } from 'dotenv';
 
 config();
 console.log(process.env.DB_URI);
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
+                                                                                                                                       
 const url = process.env.DB_URI;
 
-// Connect to your Atlas cluster
 const client = new MongoClient(url);
 
-async function run() {
+async function testConnection() {
     try {
         await client.connect();
         console.log("Successfully connected to Atlas");
@@ -23,4 +21,52 @@ async function run() {
     }
 }
 
-export default { run };
+async function getAllCustomers() {
+    try {
+        const connection = await client.connect();
+        const db = await connection.db("Training");
+        const collection = await db.collection("Customers");
+
+        const customers = await collection.find().toArray();
+        return customers;
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+async function getCustomer(id) {
+    try {
+        const connection = await client.connect();
+        const db = await connection.db("Training");
+        const collection = await db.collection("Customers");
+
+        const customers = await collection.findOne({"_id": new ObjectId(id)});
+        return customers;
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+async function deleteCustomer(id) {
+    try {
+        const connection = await client.connect();
+        const db = await connection.db("Training");
+        const collection = await db.collection("Customers");
+
+        const customers = await collection.findOne({"_id": new ObjectId(id)});
+        return customers;
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+export default { testConnection, getAllCustomers, getCustomer };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,52 @@
 import database from "./database.js";
+import express from "express"
+import cors from "cors";
+import { config } from 'dotenv';
 
-database.run().catch(console.dir);
+config();
+
+const app = express()
+
+app.use(cors())
+
+app.get("/customers", async function(req, res) {
+    try {
+        const customers = await database.getAllCustomers();
+        res.send(customers).statusCode(200);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+app.get("/customers/:id", async function(req, res) {
+    try {
+        const customer = await database.getCustomer(req.params.id);
+        if (customer) {
+            res.send(customer).statusCode(200);
+        } else {
+            res.send("Customer not found").statusCode(404);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+})
+
+app.delete("/customers/:id", function(req, res) {
+    
+})
+
+app.use(express.json());
+
+app.post("/customers", function(req, res) {
+
+});
+
+app.put("/customers/:id", function(req, res) {
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server listening on port " + process.env.PORT);
+});
