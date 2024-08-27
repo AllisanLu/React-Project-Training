51f0c94f1fde38dceb3ad98fd77a1d888a2cdf0e
AllisanLu <alcp2610@gmail.com>
2024-08-27 14:17:19 -0400

    Fix POST and PUt

--- client/front-end-app/src/services/CustomerServices.js
+++ client/front-end-app/src/services/CustomerServices.js
@@ -4,17 +4,20 @@ export const getCustomers = async () => {
 };
 
 export const addCustomer = async(customer) => {
+  console.log(customer);
   const response = await fetch("http://localhost:4000/customers", {
+    headers: {"Content-Type": "application/json"},
     method: "POST",
-    body: customer
+    body: JSON.stringify(customer)
   })
   return response;
 }
 
 export const updateCustomer = async(id, customer) => {
   const response = await fetch(`http://localhost:4000/customers/${id}`, {
+    headers: {"Content-Type": "application/json"},
     method: "PUT",
-    body: customer
+    body: JSON.stringify(customer)
   })
   return response
 }
