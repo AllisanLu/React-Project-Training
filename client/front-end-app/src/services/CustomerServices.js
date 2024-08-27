56ceb502ed1510f3ee4cef21404103b0cfb75efc
AllisanLu <alcp2610@gmail.com>
2024-08-27 13:40:48 -0400

    Customer Services

--- client/front-end-app/src/services/CustomerServices.js
+++ client/front-end-app/src/services/CustomerServices.js
@@ -2,3 +2,25 @@ export const getCustomers = async () => {
   const response = await fetch("http://localhost:4000/customers");
   return response;
 };
+
+export const addCustomer = async(customer) => {
+  const response = await fetch("http://localhost:4000/customers", {
+    method: "POST",
+    body: customer
+  })
+  return response;
+}
+
+export const updateCustomer = async(id, customer) => {
+  const response = await fetch(`http://localhost:4000/customers/${id}`, {
+    method: "PUT",
+    body: customer
+  })
+  return response
+}
+
+export const deleteCustomer = async(id) => {
+  const response = await fetch(`http://localhost:4000/customers/${id}`, {
+    method: "DELETE",
+  })
+}
